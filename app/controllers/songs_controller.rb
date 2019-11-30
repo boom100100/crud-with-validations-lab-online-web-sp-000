e450720ae85fe658a913ecf7e25eb3d12a83c6a8
Bernadette Davis <bsd231@nyu.edu>
2019-11-30 20:45:01 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,24 @@
 class SongsController < ApplicationController
+  def index
+    @songs = Song.all
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new
+
+    if @song.valid?
+      @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
+  end
+
+  def update
+    
+  end
 end
