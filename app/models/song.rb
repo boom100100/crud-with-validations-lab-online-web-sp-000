192ea2687e2be4462ad2fa7526ee30c72cb28e29
Bernadette Davis <bsd231@nyu.edu>
2019-11-30 22:41:40 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,8 +1,9 @@
 class Song < ApplicationRecord
   validates :title, presence: true
-  validates :released, presence: true
-  validates :release_year, presence: true #if :released #== true
-
-  #validates :release_year, presence: false if !:released# == false
+  validates :title, uniqueness: {
+    scope: %i[release_year artist_name],
+    message: 'cannot be repeated by the same artist in the same year'
+  }
+  validates :released, inclusion: { in: [true, false] }
   validates :artist_name, presence: true
 end
