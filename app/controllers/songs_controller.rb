e2dfc21b84ac84ad282f27db3df0b5f8adce21a0
Bernadette Davis <bsd231@nyu.edu>
2019-11-30 21:12:04 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -29,7 +29,7 @@ class SongsController < ApplicationController
   def update
     @song = Song.find(params[:id])
 
-    if @song.update(song_params
+    if @song.update(song_params)
       redirect_to song_path(@song)
     else
       render :edit
