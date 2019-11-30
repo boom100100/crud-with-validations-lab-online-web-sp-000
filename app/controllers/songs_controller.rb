3a5b90680ab2048909bd2e84bd0471c28929dd71
Bernadette Davis <bsd231@nyu.edu>
2019-11-30 21:09:47 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -12,7 +12,7 @@ class SongsController < ApplicationController
   end
 
   def create
-    @song = Song.new(song_params)
+    @song = Song.new(song_params(params))
 
     if @song.valid?
       @song.save
