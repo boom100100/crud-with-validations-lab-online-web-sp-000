468392cc826b356d2176bdaf149849d07b816fa7
Bernadette Davis <bsd231@nyu.edu>
2019-11-30 21:37:16 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,10 +1,7 @@
 class Song < ApplicationRecord
   validates :title, presence: true
   validates :released, presence: true
-  if :released
-    validates :release_year, presence: true
-  else
-    validates :release_year, presence: false
-  end
+  validates :release_year, presence: true if :released
+  #validates :release_year, presence: false if !:released
   validates :artist_name, presence: true
 end
